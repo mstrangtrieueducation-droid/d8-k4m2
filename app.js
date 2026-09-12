127b8584f10ecdac9b2d679265200295930e8207
mstrangtrieueducation-droid <310901278+mstrangtrieueducation-droid@users.noreply.github.com>
2026-09-12 10:51:18 +0700

    Add Debate 2 lesson 24 using existing debate-day workflow

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ SITE_MAP["d2-22-b4e719c6"]="2-22";
 COURSES[1].lessons.push({"course":2,"number":23,"unit":8,"topic":"Unit 8 - Asking for Advice","type":"writing","slideId":"1wN59JjYdkWXtBV9tIeSxS0cn_qi-bU7Y3TBTQxLWKDY","slideUrl":"https://docs.google.com/presentation/d/1wN59JjYdkWXtBV9tIeSxS0cn_qi-bU7Y3TBTQxLWKDY/edit?usp=sharing","videos":[],"sourceLabel":"Debate 2 - Tiết 23"});
 SITE_MAP["d2-23-37c1e8a4"]="2-23";
 
+COURSES[1].lessons.push({"course":2,"number":24,"unit":8,"topic":"Unit 8 - Asking for Advice","type":"debate","slideId":"13l1oFx33CLMaNofAIAJ4vSfntvb2jSpUH3OnI9tEdNs","slideUrl":null,"videos":[],"sourceLabel":"Debate 2 - Tiết 24"});
+SITE_MAP["d2-24-58c94601"]="2-24";
+
 function escapeHtml(value){return String(value ?? "").replace(/[&<>"']/g,ch=>({"&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;","'":"&#39;"})[ch]);}
 function cleanTopic(topic){return topic.replace(/^Unit\s*\d+\s*-\s*/i,"").trim();}
 function lessonKey(){
